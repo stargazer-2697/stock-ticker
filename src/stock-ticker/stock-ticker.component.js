f72204e51f7c2cdc557d0ba2b5fb72b41e4b2dbf
process-execution <process-execution@users.noreply.github.com>
2018-10-05 01:38:27 +0800

    Ignore generated source in all /src subfolders

--- src/stock-ticker/stock-ticker.component.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class StockTickerComponent {
-}
-exports.default = new StockTickerComponent();
-//# sourceMappingURL=stock-ticker.component.js.map
